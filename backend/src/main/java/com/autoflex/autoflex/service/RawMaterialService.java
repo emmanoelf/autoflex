eeb903b74e4359df2a1d6f67aad85612056adaa0
emmanoel <emmanoel_95@hotmail.com>
2026-02-26 19:58:20 -0300

    feat(RawMaterial): add service findById

--- backend/src/main/java/com/autoflex/autoflex/service/RawMaterialService.java
+++ backend/src/main/java/com/autoflex/autoflex/service/RawMaterialService.java
@@ -3,6 +3,9 @@ package com.autoflex.autoflex.service;
 import com.autoflex.autoflex.dto.RawMaterialDTO;
 import com.autoflex.autoflex.dto.RawMaterialResponseDTO;
 
+import java.util.UUID;
+
 public interface RawMaterialService {
     RawMaterialResponseDTO save(RawMaterialDTO rawMaterialDTO);
+    RawMaterialResponseDTO findById(UUID rawMaterialID);
 }
--- backend/src/main/java/com/autoflex/autoflex/service/impl/RawMaterialServiceImpl.java
+++ backend/src/main/java/com/autoflex/autoflex/service/impl/RawMaterialServiceImpl.java
@@ -2,12 +2,15 @@ package com.autoflex.autoflex.service.impl;
 
 import com.autoflex.autoflex.dto.RawMaterialDTO;
 import com.autoflex.autoflex.dto.RawMaterialResponseDTO;
+import com.autoflex.autoflex.exception.NotFoundException;
 import com.autoflex.autoflex.mapper.RawMaterialMapper;
 import com.autoflex.autoflex.model.RawMaterial;
 import com.autoflex.autoflex.repository.RawMaterialRepository;
 import com.autoflex.autoflex.service.RawMaterialService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +26,12 @@ public class RawMaterialServiceImpl implements RawMaterialService {
         RawMaterial rawMaterial = RawMaterialMapper.toModel(rawMaterialDTO);
         RawMaterial savedRawMaterial = this.rawMaterialRepository.saveAndFlush(rawMaterial);
         return RawMaterialMapper.toDTO(savedRawMaterial);
+    }
+
+    @Override
+    public RawMaterialResponseDTO findById(UUID rawMaterialID) {
+        return this.rawMaterialRepository.findById(rawMaterialID)
+                .map(RawMaterialMapper::toDTO)
+                .orElseThrow(() -> new NotFoundException("Raw material not found"));
     }
 }
