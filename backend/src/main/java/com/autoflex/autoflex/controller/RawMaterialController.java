91507a35434c589c259970a2caa094f8c6133054
emmanoel <emmanoel_95@hotmail.com>
2026-02-25 23:44:50 -0300

    docs(RawMaterial): add swagger annotations for save endpoint

---
+++ backend/src/main/java/com/autoflex/autoflex/controller/RawMaterialController.java
@@ -0,0 +1,24 @@
+package com.autoflex.autoflex.controller;
+
+import com.autoflex.autoflex.dto.RawMaterialDTO;
+import com.autoflex.autoflex.dto.RawMaterialResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "Raw Material Controller")
+public interface RawMaterialController {
+
+    @Operation(summary = "Create raw material")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Raw material create successfully",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RawMaterialDTO.class))})
+    })
+    ResponseEntity<RawMaterialResponseDTO> save(RawMaterialDTO rawMaterialDTO);
+}
